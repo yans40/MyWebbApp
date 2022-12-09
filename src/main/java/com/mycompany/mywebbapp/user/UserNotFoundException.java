764c454c21528bd18795273f2f7c23b965732eb0
yannick DOLLOU <yannick.dollou@axa.fr>
2022-12-09 13:07:35 +0100

    -mise en place de la méthode update et delete

---
+++ src/main/java/com/mycompany/mywebbapp/user/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package com.mycompany.mywebbapp.user;
+
+public class UserNotFoundException extends Throwable {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/mycompany/mywebbapp/user/UserService.java
+++ src/main/java/com/mycompany/mywebbapp/user/UserService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -17,5 +18,22 @@ public class UserService {
     public void save(User user) {
 
         userRepository.save(user);
+    }
+
+    public User get(Integer id) throws UserNotFoundException {
+     Optional<User> result = userRepository.findById(id);
+     if(result.isPresent()){
+         return result.get();
+     }
+     throw new UserNotFoundException("Could not find any users with ID"+ id);
+    }
+
+    public void delete(Integer id) throws UserNotFoundException {
+       Long count= userRepository.countById(id);
+       if (count== null || count ==0){
+
+           throw new UserNotFoundException("Could not find any users with ID"+ id);
+       }
+        userRepository.deleteById(id);
     }
 }
